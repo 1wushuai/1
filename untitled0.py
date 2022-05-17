895074e327deaf9c92a5b6972dba4a973c81ed84
1wushuai <105718632+1wushuai@users.noreply.github.com>
2022-05-17 15:03:35 +0200

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 17 14:01:48 2022
+
+@author: lenovo
+"""
+
+def add():
+    num1=2
+    num2=3
+    result=num1+num2
+    print('%d+%d=%d'%(num1,num2,result))
+add()    
